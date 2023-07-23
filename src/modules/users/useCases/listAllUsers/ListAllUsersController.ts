173bc1e0f62e20d73a79e06f96f7008c78e7e418
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-07-23 16:44:45 +0100

    added: controller that show all users

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      const users = this.listAllUsersUseCase.execute({ user_id: user_id?.[0] });
+
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
